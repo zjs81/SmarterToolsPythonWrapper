4caada4836370d2219a89a3750ea1830b6f50786
zjs81 <30362347+zjs81@users.noreply.github.com>
2023-03-27 18:26:59 -0700

    Update Example_Functions.py
    Made easier to understand

--- api/Example_Functions.py
+++ api/Example_Functions.py
@@ -1,17 +1,20 @@
-import smapi
+from smapi import SMAPI
 
-me = smapi.SMAPI("admin","admin","http://smurl.com")
+me = SMAPI("admin","adminpassword","https://mail.mailserver.com")
 
-me.auth()
 
-#System Admin Only Calls
-#me.GetUser("admin@example.com")
-#me.SystemDomainDetails("example.com")
-#me.SystemExportDomainsList()
-#me.SystemExportDomainsListToFile("test.csv")
+
+# System Admin Only Calls
+print(me.get_user("admin@domain.com"))
+print(me.system_domain_details("domain.com"))
+print(me.system_export_domains_list())
+print(me.system_export_domains_list_to_file("test.csv"))
+
+me = SMAPI("admin@domain","pass","https://mail.domain.com")
+
 #Domain Admin Only Calls
-#me.GetDomain()
-#me.GetDomainPermissions()
-#me.GetTotalDomainUsers()
-#me.GetAlias("testing")
-#me.DomainAccountListCount()
+print(me.get_domain())
+print(me.get_domain_permissions())
+print(me.get_total_domain_users())
+print(me.get_alias("testing"))
+print(me.domain_account_list_count())
